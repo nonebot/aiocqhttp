3ae75cacefd85dd4fd677277e08727286fe6c03b
Richard Chien <richardchienthebest@gmail.com>
2020-01-28 15:33:07 +0800

    Update README.md

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from aiocqhttp import CQHttp, ApiError
+from aiocqhttp import CQHttp, ApiError, Event
 
 bot = CQHttp(api_root='http://127.0.0.1:5700/',
              access_token='123',
@@ -7,32 +7,33 @@ bot = CQHttp(api_root='http://127.0.0.1:5700/',
 
 @bot.on_message
 # 上面这句等价于 @bot.on('message')
-async def handle_msg(context):
-    # 下面这句等价于 bot.send_private_msg(user_id=context['user_id'],
-    #                                  message='你好呀，下面一条是你刚刚发的：')
+async def handle_msg(event: Event):
     try:
-        await bot.send(context, '你好呀，下面一条是你刚刚发的：')
+        # 下面这句等价于 bot.send_private_msg(user_id=event.user_id,
+        #                                  message='你好呀，下面一条是你刚刚发的：')
+        await bot.send(event, '你好呀，下面一条是你刚刚发的：')
     except ApiError:
         pass
-    return {'reply': context['message'],
-            'at_sender': False}  # 返回给 HTTP API 插件，走快速回复途径
+
+    # 返回给 HTTP API 插件，走快速回复途径
+    return {'reply': event.message, 'at_sender': False}
 
 
 @bot.on_notice('group_increase')
 # 上面这句等价于 @bot.on('notice.group_increase')
-async def handle_group_increase(context):
-    info = await bot.get_group_member_info(group_id=context['group_id'],
-                                           user_id=context['user_id'])
+async def handle_group_increase(event: Event):
+    info = await bot.get_group_member_info(group_id=event.group_id,
+                                           user_id=event.user_id)
     nickname = info['nickname']
     name = nickname if nickname else '新人'
-    await bot.send(context, message='欢迎{}～'.format(name),
+    await bot.send(event, message=f'欢迎{name}～',
                    at_sender=True, auto_escape=True)
 
 
 @bot.on_request('group', 'friend')
 # 上面这句等价于 @bot.on('request.group', 'request.friend')
-async def handle_group_request(context):
-    if context['message'] != 'some-secret':
+async def handle_group_request(event: Event):
+    if event.comment != 'some-secret':
         # 验证信息不符，拒绝
         return {'approve': False, 'reason': '你填写的验证信息有误'}
     return {'approve': True}
