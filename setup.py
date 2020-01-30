e1bd25e856ce5d16b9873612cc4011847948b8d2
Richard Chien <richardchienthebest@gmail.com>
2020-01-30 19:16:35 +0800

    Bump version to v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='aiocqhttp',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(include=('aiocqhttp', 'aiocqhttp.*')),
     url='https://github.com/cqmoe/python-aiocqhttp',
     license='MIT License',
@@ -14,15 +14,16 @@ setup(
     description='A Python SDK with async I/O for CQHTTP.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['Quart>=0.10', 'aiohttp>=3.6'],
+    install_requires=['Quart>=0.10,<1.0', 'aiohttp>=3.6'],
     extras_require={
         'all': ['ujson'],
     },
     python_requires='>=3.7',
     platforms='any',
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Robot Framework',
     ),
 )
