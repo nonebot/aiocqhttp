b3c542c915cbaef683137821cf8c6125dab4caff
Richard Chien <richardchienthebest@gmail.com>
2020-02-09 10:44:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, findall
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
+packages = find_packages(include=('aiocqhttp', 'aiocqhttp.*'))
+stub_files = list(filter(lambda x: x.endswith('.pyi'), findall('aiocqhttp')))
+
 setup(
     name='aiocqhttp',
     version='1.0.1',
-    packages=find_packages(include=('aiocqhttp', 'aiocqhttp.*')),
+    packages=packages,
+    data_files=stub_files,
     url='https://github.com/cqmoe/python-aiocqhttp',
     license='MIT License',
     author='Richard Chien',
@@ -17,9 +21,6 @@ setup(
     install_requires=['Quart>=0.10,<1.0', 'aiohttp>=3.6'],
     extras_require={
         'all': ['ujson'],
-    },
-    package_data={
-        "": ["*.pyi"],
     },
     python_requires='>=3.7',
     platforms='any',
