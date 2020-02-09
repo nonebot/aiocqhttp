006d60a08d3475739b82bdb00e7a695126083520
Richard Chien <richardchienthebest@gmail.com>
2020-02-09 14:01:27 +0800

    Move script

--- scripts/build_module_api_docs.py
+++ scripts/build_module_api_docs.py
@@ -1,10 +1,12 @@
 import os
 import sys
 
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
 os.system(sys.executable +
           ' -m pdoc'
           '   --html'
-          '   -o docs/module'
+          '   -o docs/module'  # cwd should be project dir
           '   -c html_lang="\'zh\'"'
           '   -c show_type_annotations=True'
           '   -c show_inherited_members=True'
