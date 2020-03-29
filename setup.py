0c70da8793de2eae54fa4b958bd46e5c4a3b36d2
Richard Chien <richardchienthebest@gmail.com>
2020-03-29 12:58:54 +0800

    bump version to v1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='aiocqhttp',
-    version='1.2.3',
+    version='1.2.4',
     url='https://github.com/cqmoe/python-aiocqhttp',
     license='MIT License',
     author='Richard Chien',
