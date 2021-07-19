3f64ef0e173b62a17183c79d953f9f8952a5e851
Richard Chien <richardchienthebest@gmail.com>
2021-07-19 23:27:16 +0800

    use black to check code style instead of flake8

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='aiocqhttp',
-    version='1.4.1',
-    url='https://github.com/nonebot/aiocqhttp',
-    license='MIT License',
-    author='Richard Chien',
-    author_email='richardchienthebest@gmail.com',
-    maintainer='NoneBot Team',
-    description='A Python SDK with async I/O for OneBot (CQHTTP).',
+    name="aiocqhttp",
+    version="1.4.1",
+    url="https://github.com/nonebot/aiocqhttp",
+    license="MIT License",
+    author="Richard Chien",
+    author_email="richardchienthebest@gmail.com",
+    maintainer="NoneBot Team",
+    description="A Python SDK with async I/O for OneBot (CQHTTP).",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(include=('aiocqhttp', 'aiocqhttp.*')),
+    packages=find_packages(include=("aiocqhttp", "aiocqhttp.*")),
     package_data={
-        '': ['*.pyi'],
+        "": ["*.pyi"],
     },
-    install_requires=['Quart>=0.14,<0.15', 'httpx>=0.11,<1.0'],
+    install_requires=["Quart>=0.14,<0.15", "httpx>=0.11,<1.0"],
     extras_require={
-        'all': ['ujson'],
+        "all": ["ujson"],
     },
-    python_requires='>=3.7',
-    platforms='any',
+    python_requires=">=3.7",
+    platforms="any",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Framework :: Robot Framework',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Robot Framework",
     ],
 )
